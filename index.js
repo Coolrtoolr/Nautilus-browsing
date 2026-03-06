4e61633f10f455a6e46fad65b099ea85013c1fa7
Coolrtoolr <Minecraftfireboy9@gmail.com>
2026-03-06 03:43:26 +0000

    modified:   index.js: changed directory to the root of the project and added a simple Express server setup.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve your HTML files from a folder named 'public'
-app.use(express.static('public'));
+// This tells the server to serve files from the current directory
+app.use(express.static(__dirname));
+
+// This ensures that when someone visits your site, they see the index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
