b0216dc13711de00c101ac5f1892c0d7e8c50d18
Coolrtoolr <Minecraftfireboy9@gmail.com>
2026-03-06 02:22:01 +0000

    modified:   Index.js     fixed missing curly bracket

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const addressBar = document.getElementById("address-bar");
 const loadBtn = document.getElementById("load-btn");
 const viewport = document.getElementById("veiwport-frame");
 
-loadBtn.addEventListener("click", () =>
+loadBtn.addEventListener("click", () => {
   let url = addressBar.value;
   if (!url.startsWith("http")) {
     url = "https://" + url;
