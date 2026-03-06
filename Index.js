5ca0529b3fd4da1a2261614534460f067f743ec6
Coolrtoolr <Minecraftfireboy9@gmail.com>
2026-03-05 18:04:03 -0800

    Implement URL loading functionality with event listeners
    Removed the test function and added event listeners for loading a URL.

--- Index.js
+++ Index.js
@@ -1,4 +1,11 @@
-function test () {
-  console.log("working");
-};
-test();
+const addressBar = document.getElementById("address-bar");
+const loadBtn = document.getElementById("load-btn");
+const viewport = document.getElementById("veiwport-frame");
+
+loadBtn.addEventListener("click", () =>
+  let url = addressBar.value;
+  if (!url.startsWith("http")) {
+    url = "https://" + url;
+  }
+  veiwport.src = url;
+});
